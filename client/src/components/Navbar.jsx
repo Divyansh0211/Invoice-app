f8a4272a83121ae6c4ab4d7bc1aced7d1cc74b82
Divyansh Dogra <divyanshdogra5881@gmail.com>
2026-02-18 15:13:14 +0530

    refactor: Remove settings link from navbar dropdown

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -17,7 +17,7 @@ const Navbar = () => {
                     <i className="fas fa-user-circle"></i> {user && user.name} <i className="fas fa-caret-down"></i>
                 </button>
                 <div className="dropdown-content">
-                    <Link to="/settings" className="text-dark">Settings</Link>
+
                     <a onClick={onLogout} href="#!" className="text-danger">
                         <i className="fas fa-sign-out-alt"></i> Logout
                     </a>
